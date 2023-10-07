2807dc4479c358081693f26fb6315a8de589cda6
Kynes <kamilpirog98@gmail.com>
2023-10-07 15:43:57 +0200

    implement crosshair renderers for cue ball and touched ball

--- Assets/Scripts/Cue/CueCrosshair.cs
+++ Assets/Scripts/Cue/CueCrosshair.cs
@@ -5,23 +5,24 @@ namespace Cue
     [RequireComponent(typeof(LineRenderer))]
     public class CueCrosshair : MonoBehaviour
     {
-        private const float ZOffsetMultiplier = 0.001f;
-        private const float VelocityMultiplier = 999f;
-        private static Vector3 ZOffset => Vector3.forward * ZOffsetMultiplier;
-
         [Header("Renderers")]
         [SerializeField] private LineRenderer cueBallRenderer;
         [SerializeField] private LineRenderer touchedBallRenderer;
+        [SerializeField] private LineRenderer circleRenderer;
 
         [Header("Reflection Settings")]
-        [SerializeField] private float cueBallPredictionMultiplier;
+        [SerializeField] private float lineLenghtMultiplier;
+        [SerializeField] private float minOffsetToStraightLine;
         [SerializeField] private LayerMask layerMask;
 
-        [Header("References")]
-        [SerializeField] private Collider2D cueBall;
+        [Header("References")] 
+        [SerializeField] private CircleCollider2D cueBall;
 
-        private Vector3 _center;
-        private float CircleRadius => (cueBall.bounds.extents.x * 2f) * cueBall.transform.localScale.x;
+        [Header("Circle render")]
+        [SerializeField] private int edgesCount;
+
+        private Vector3 _ballCenter;
+        private float CircleRadius => cueBall.radius * cueBall.transform.lossyScale.x;
         
         private void Awake()
         {
@@ -37,37 +38,41 @@ namespace Cue
         {
             DisableRenderers();
         }
-
+        
         private void Update()
         {
-            var hit = Physics2D.CircleCast(_center, CircleRadius, transform.right, Mathf.Infinity, layerMask);
+            var hit = PerformCircleCast();
 
-            if (!hit.collider) 
+            if (!hit.collider)
                 return;
-
+            
             Vector3 collisionPoint = hit.point;
             Vector3 collisionNormal = hit.normal;
+
+            DrawCueBallRenderer(hit, collisionPoint, collisionNormal);
+            DrawTouchedBallRenderer(hit, collisionPoint, collisionNormal);
+        }
+
+        private RaycastHit2D PerformCircleCast()
+        {
+            return Physics2D.CircleCast(_ballCenter, CircleRadius, transform.right, Mathf.Infinity, layerMask);
+        }
+
+        private void DrawCueBallRenderer(RaycastHit2D hit, Vector3 collisionPoint, Vector3 collisionNormal)
+        {
+            cueBallRenderer.positionCount = 2;
+            cueBallRenderer.SetPosition(0, _ballCenter);
             
-            UpdateCueBallRenderer(collisionPoint, collisionNormal);
-            UpdateTouchedBallRenderer(hit, collisionPoint, collisionNormal);
+            var circleCenter = collisionPoint + collisionNormal * CircleRadius;
+            cueBallRenderer.SetPosition(1, circleCenter);
+            
+            DrawCircleRenderer(circleCenter);
+            DrawCueBallReflection(hit, collisionPoint, collisionNormal);
         }
 
-        private void UpdateCueBallRenderer(Vector3 collisionPoint, Vector3 collisionNormal)
+        private void DrawTouchedBallRenderer(RaycastHit2D hit, Vector3 collisionPoint, Vector3 collisionNormal)
         {
-            cueBallRenderer.SetPosition(0, _center - ZOffset);
-            cueBallRenderer.SetPosition(1, collisionPoint - ZOffset);
-
-            var velocity = (collisionPoint - cueBall.transform.position) * VelocityMultiplier;
-
-            Vector3 cueBallReflection = Vector2.Reflect(velocity, collisionNormal).normalized;
-            cueBallReflection *= cueBallPredictionMultiplier;
-
-            cueBallRenderer.SetPosition(2, collisionPoint + cueBallReflection);
-        }
-
-        private void UpdateTouchedBallRenderer(RaycastHit2D hit, Vector3 collisionPoint, Vector3 collisionNormal)
-        {
-            if (hit.collider.gameObject.layer != LayerMask.NameToLayer("StandardBall"))
+            if (!IsStandardBall(hit.collider))
             {
                 touchedBallRenderer.enabled = false;
                 return;
@@ -76,26 +81,78 @@ namespace Cue
             var touchedBallPosition = hit.transform.position;
             
             touchedBallRenderer.enabled = true;
-            touchedBallRenderer.SetPosition(0, touchedBallPosition - ZOffset);
-            touchedBallRenderer.SetPosition(1, (collisionPoint - collisionNormal) - ZOffset);
+            touchedBallRenderer.SetPosition(0, touchedBallPosition);
+
+            var touchedBallDirection = (collisionPoint - collisionNormal);
+            var touchedBallReflection = (touchedBallDirection - touchedBallPosition).normalized;
+            
+            var offsetToStraightLine = Vector3.Dot(transform.right, touchedBallReflection.normalized);
+            touchedBallReflection *= Mathf.Abs(offsetToStraightLine) * lineLenghtMultiplier;
+            
+            touchedBallRenderer.SetPosition(1, touchedBallPosition + touchedBallReflection);
+        }
+
+        private void DrawCueBallReflection(RaycastHit2D hit, Vector3 collisionPoint, Vector3 collisionNormal)
+        {
+            if (!IsStandardBall(hit.collider))
+                return;
+
+            cueBallRenderer.positionCount = 3;
+
+            var velocity = (collisionPoint - _ballCenter).normalized;
+            var cueBallReflection = Vector3.Reflect(velocity, collisionNormal).normalized;
+
+            var offsetToStraightLine = Vector3.Dot(transform.up, cueBallReflection);
+            cueBallReflection *= Mathf.Abs(offsetToStraightLine) * lineLenghtMultiplier;
+
+            if (Mathf.Abs(offsetToStraightLine) < minOffsetToStraightLine)
+            {
+                cueBallRenderer.positionCount = 2;
+                return;
+            }
+
+            cueBallRenderer.SetPosition(2, collisionPoint + cueBallReflection);
+        }
+
+        private static bool IsStandardBall(Collider2D collider)
+        {
+            return collider.gameObject.layer == LayerMask.NameToLayer("StandardBall");
+        }
+
+        private void DrawCircleRenderer(Vector3 circleCenter)
+        {
+            for (var i = 0; i < edgesCount; i++)
+            {
+                var progress = (float)i / (edgesCount - 1);
+                var radians = progress * 2f * Mathf.PI;
+
+                var cos = Mathf.Cos(radians) * CircleRadius;
+                var sin = Mathf.Sin(radians) * CircleRadius;
+
+                var edgePosition = new Vector3(cos, sin, 0f) + circleCenter;
+                circleRenderer.SetPosition(i, edgePosition);
+            }
         }
 
         private void InitializeRenderers()
         {
             cueBallRenderer.positionCount = 3;
             touchedBallRenderer.positionCount = 2;
+            circleRenderer.positionCount = edgesCount;
         }
 
         private void SetupInitialValues()
         {
-            _center = cueBall.transform.position;
+            _ballCenter = cueBall.transform.position;
             cueBallRenderer.enabled = true;
+            circleRenderer.enabled = true;
             touchedBallRenderer.enabled = false;
         }
 
         private void DisableRenderers()
         {
             cueBallRenderer.enabled = false;
+            circleRenderer.enabled = false;
             touchedBallRenderer.enabled = false;
         }
     }
