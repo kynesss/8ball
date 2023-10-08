b1435d84fd72a18a8eab89d07769d84c85a91a05
Kynes <kamilpirog98@gmail.com>
2023-10-08 16:43:14 +0200

    pull up movement logic from CueController to CueMovementHandler, rename CueController to CueInputController

---
+++ Assets/Scripts/Cue/CueInputController.cs
@@ -0,0 +1,79 @@
+﻿using Balls;
+using Cue.Dragging;
+using Cue.Movement;
+using UnityEngine;
+
+namespace Cue
+{
+    public class CueController : MonoBehaviour
+    {
+        [Header("References")] 
+        [SerializeField] private GameObject sprite;
+
+        private CueCrosshair _crosshair;
+        private CueDragHandler _dragHandler;
+        private CueMovementHandler _movementHandler;
+        
+        private Vector2 _aimDirection;
+        private bool _enabled;
+
+        private void Awake()
+        {
+            _crosshair = GetComponent<CueCrosshair>();
+            _dragHandler = GetComponent<CueDragHandler>();
+            Enable();
+        }
+
+        private void Enable()
+        {
+            sprite.SetActive(true);
+            
+            _enabled = true;
+            _crosshair.enabled = true;
+            _dragHandler.enabled = true;
+            _movementHandler.enabled = true;
+        }
+
+        private void Disable()
+        {
+            sprite.SetActive(false);
+            
+            _enabled = false;
+            _crosshair.enabled = false;
+            _dragHandler.enabled = false;
+            _movementHandler.enabled = false;
+        }
+
+        private void Update()
+        {
+            if (BallController.AllBallsAreStationary && !_enabled)
+                Enable();
+
+            if (!_enabled)
+                return;
+
+            var dragStrength = _dragHandler.DragStrength;
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                _aimDirection = _movementHandler.MovementDirection;   
+                _dragHandler.BeginDrag();
+            }
+
+            if (Input.GetMouseButton(0))
+                _dragHandler.Drag();
+            else
+                _movementHandler.CalculateAngles();
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _dragHandler.EndDrag();
+                _movementHandler.Hit(_aimDirection, dragStrength);
+                
+                Disable();
+            }
+            else
+                _movementHandler.HandleMovement(dragStrength);
+        }
+    }
+}
---
+++ Assets/Scripts/Cue/Movement/CueMovementHandler.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+using UnityEngine;
+
+namespace Cue.Movement
+{
+    public class CueMovementHandler : MonoBehaviour
+    {
+        [SerializeField] private float strengthMultiplier;
+        [SerializeField] private Rigidbody2D cueBall;
+
+        private float _radians;
+        private float _degrees;
+        
+        public Vector2 MovementDirection { get; private set; }
+
+        private void OnEnable()
+        {
+            ResetPosition();
+        }
+
+        private void Update()
+        {
+            CalculateMovementDirection();
+        }
+
+        private void ResetPosition()
+        {
+            transform.position = cueBall.position;
+        }
+
+        private void CalculateMovementDirection()
+        {
+            MovementDirection = (MouseController.GetWorldPosition() - cueBall.position).normalized;
+        }
+
+        public void CalculateAngles()
+        {
+            _radians = Mathf.Atan2(MovementDirection.y, MovementDirection.x);
+            _degrees = _radians * Mathf.Rad2Deg;
+        }
+
+        public void HandleMovement(float dragStrength)
+        {
+            var cueBallPosition = cueBall.position;
+
+            var horizontal = cueBallPosition.x - Mathf.Cos(_radians) * dragStrength;
+            var vertical = cueBallPosition.y - Mathf.Sin(_radians) * dragStrength;
+
+            transform.position = new Vector2(horizontal, vertical);
+            transform.rotation = Quaternion.Euler(0f, 0f, _degrees);
+        }
+
+        public void Hit(Vector2 direction, float dragStrength)
+        {
+            var strength = dragStrength * strengthMultiplier;
+            cueBall.AddForce(direction * strength, ForceMode2D.Impulse);
+        }
+    }
+}
