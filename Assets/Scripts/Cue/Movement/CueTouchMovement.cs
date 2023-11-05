ebcaecb4d39f44333cda05f503d1c489230193eb
Kynes <kamilpirog98@gmail.com>
2023-11-05 12:34:40 +0100

    show nonserializedfields in inspector

--- Assets/Scripts/Cue/Movement/CueTouchMovement.cs
+++ Assets/Scripts/Cue/Movement/CueTouchMovement.cs
@@ -2,20 +2,20 @@
 using Common;
 using Cue.Dragging;
 using Medicine;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Cue.Movement
 {
     public class CueTouchMovement : MonoBehaviour, IMovementHandler
-    {
+    {   
         [SerializeField] private float xOffset;
         [SerializeField] private float speedMultiplier;
-        
         [Inject.Single] private WhiteBall WhiteBall { get; }
         [Inject] private IDragHandler DragHandler { get; }
 
-        private float _degrees;
-        private float _radians;
+        [ShowNonSerializedField] private float _degrees;
+        [ShowNonSerializedField] private float _radians;
         private Vector2 Center => WhiteBall.transform.position;
         
         private void OnEnable()
